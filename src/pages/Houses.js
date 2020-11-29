c8c2176ce9db8d1a1b7909a48155c8e48e1afcd4
Christofferos <kristopher.werlinder@telia.com>
2020-11-29 17:01:51 +0100

    Update

--- src/pages/Houses.js
+++ src/pages/Houses.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { useState, useEffect } from "react";
+/*
+import { useState, useEffect } from "react"; */
 import Map from "./../components/Map";
 
 function Houses() {
